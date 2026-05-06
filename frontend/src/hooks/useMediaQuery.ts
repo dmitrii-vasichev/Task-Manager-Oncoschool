f3e67a76a1274b7c03309aa8ae37389233fe8e73
Dmitrii Vasichev <dmitrii-vasichev@users.noreply.github.com>
2026-05-06 11:33:49 -0600

    feat: add media query hook

---
+++ frontend/src/hooks/useMediaQuery.ts
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+
+    function updateMatches() {
+      setMatches(mediaQueryList.matches);
+    }
+
+    updateMatches();
+    mediaQueryList.addEventListener("change", updateMatches);
+
+    return () => {
+      mediaQueryList.removeEventListener("change", updateMatches);
+    };
+  }, [query]);
+
+  return matches;
+}
